d7d4fef1486cc92cb1cd19997ee2074f079856ea
joachimharms <joachimharms77@gmail.com>
2017-11-26 17:05:06 +0100

    Resolve merge conflict3 - done

--- C1_001_AssemblyAndManifest/C03_003_BasicDataTypes/Program.cs
+++ C1_001_AssemblyAndManifest/C03_003_BasicDataTypes/Program.cs
@@ -63,6 +63,21 @@ namespace C03_003_BasicDataTypes
             Console.WriteLine("12.GetType() = {0}", 12.GetType());
             Console.WriteLine();
         }
+
+        static void DataTypeFunctionality()
+        {
+            Console.WriteLine("=> Data type Functionality:");
+            Console.WriteLine("Max of int: {0}", int.MaxValue);
+            Console.WriteLine("Min of int: {0}", int.MinValue);
+            Console.WriteLine("Max of double: {0}", double.MaxValue);
+            Console.WriteLine("Min of double: {0}", double.MinValue);
+            Console.WriteLine("double.Epsilon: {0}", double.Epsilon);
+            Console.WriteLine("double.PositiveInfinity: {0}",
+                double.PositiveInfinity);
+            Console.WriteLine("double.NegativeInfinity: {0}",
+                double.NegativeInfinity);
+            Console.WriteLine();
+        }
     }
 
 }
