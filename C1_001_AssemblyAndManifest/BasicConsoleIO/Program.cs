9953a8e5e045af79294d1d30e21ae3adf7693e24
joachimharms <joachimharms77@gmail.com>
2017-11-26 10:25:07 +0100

    Update BasicConsoleIO - done

--- C1_001_AssemblyAndManifest/BasicConsoleIO/Program.cs
+++ C1_001_AssemblyAndManifest/BasicConsoleIO/Program.cs
@@ -1,30 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 
 namespace BasicConsoleIO
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void DisplayMessage()
         {
-            Console.WriteLine("*****Basic Console I/O*****");
-            GetUserData();
-            Console.ReadLine();
+            // Verwendung von String Format in einer WPF MessageBox:
+            var userMessage = string.Format($"100000 in hex is {100000:x}");
+
+            // You need to reference PresentationFramework.dll
+            // in order to compile this line of code:
+            MessageBox.Show(userMessage);
+        }
+
+        // Make use of some format tags.
+        private static void FormatNumericalData()
+        {
+            Console.WriteLine("The value 99999 in various formats:");
+            Console.WriteLine($"c format: {99999:C}");
+            Console.WriteLine($"d9 format: {99999:D9}");
+            Console.WriteLine($"f3 format: {99999:F3}");
+            Console.WriteLine($"n format: {999999:N}");
+
+            // Notice that upper- or lowercasing for hex
+            // determines if letters are upper- or lowercase.
+            Console.WriteLine($"E format: {99999:E}");
+            Console.WriteLine($"e format: {99999:e}");
+            Console.WriteLine($"X format: {99999:X}");
+            Console.WriteLine($"x format: {99999:x}");
         }
 
         private static void GetUserData()
         {
             // Get name and age.
             Console.WriteLine("Please enter your name: ");
-            string userName = Console.ReadLine();
+            var userName = Console.ReadLine();
             Console.WriteLine("Please enter your age: ");
-            string userAge = Console.ReadLine();
+            var userAge = Console.ReadLine();
 
             // Change echo color, just for fund.
-            ConsoleColor prevColor = Console.ForegroundColor;
+            var prevColor = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Red;
 
             // Echo to the console.
@@ -32,7 +49,19 @@ namespace BasicConsoleIO
 
             // Restore previous color.
             Console.ForegroundColor = prevColor;
+        }
 
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("*****Basic Console I/O*****");
+            GetUserData();
+            Console.WriteLine();
+
+            FormatNumericalData();
+            Console.WriteLine();
+
+            DisplayMessage();
+            Console.ReadLine();
         }
     }
 }
