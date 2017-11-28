4953304477c81f380086a229bba30b3a2daedfef
joachimharms <joachimharms77@gmail.com>
2017-11-29 00:11:41 +0100

    TODO: Add own functionality for strings

--- C1_001_AssemblyAndManifest/C03_003_BasicDataTypes/Program.cs
+++ C1_001_AssemblyAndManifest/C03_003_BasicDataTypes/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace C03_003_BasicDataTypes
 {
@@ -48,6 +49,10 @@ namespace C03_003_BasicDataTypes
 
             ParseFromStrings();
             Console.WriteLine();
+
+            UseDatesAndTimes();
+
+            UseBigInteger();
 
         }
 
@@ -121,6 +126,40 @@ namespace C03_003_BasicDataTypes
             Console.WriteLine("Value of c: {0}", c);
             Console.WriteLine();
         }
+
+        static void UseDatesAndTimes()
+        {
+            // This constructor takes (year, month, day).
+            var dt = new DateTime(2017, 01, 30);
+
+            // What day of the month is this?
+            Console.WriteLine("The day of {0} is {1}", dt.Date, dt.DayOfWeek);
+
+            // Month is now December.
+            dt = dt.AddMonths(2);
+            Console.WriteLine("Daylight savings: {0}", dt.IsDaylightSavingTime());
+
+            // This constructor takes (hours, minutes, seconds).
+            TimeSpan ts = new TimeSpan(4, 30, 0);
+            Console.WriteLine(ts);
+
+            // Subtract 15 minutes from the current TimeSpan and
+            // print the result.
+            Console.WriteLine(ts.Subtract(new TimeSpan(0, 15, 0)));
+        }
+
+        static void UseBigInteger()
+        {
+            Console.WriteLine("=> Use BigInteger:");
+            BigInteger biggy =
+                BigInteger.Parse("9999999999999999999999999999999999999999999999");
+            Console.WriteLine("Value of biggy is {0}", biggy);
+            Console.WriteLine("Is biggy an even value?: {0}", biggy.IsEven);
+            Console.WriteLine("Is biggy a power of two?: {0}", biggy.IsPowerOfTwo);
+            BigInteger reallyBig = BigInteger.Multiply(biggy,
+                BigInteger.Parse("8888888888888888888888888888888888888888888"));
+            Console.WriteLine("Value of reallyBig is {0}", reallyBig);
+        }
     }
-    
+
 }
---
+++ C1_001_AssemblyAndManifest/C03_004_MembersOfSystemString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C03_004_MembersOfSystemString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicStringFunctionality();
+        }
+
+        static void BasicStringFunctionality()
+        {
+            Console.WriteLine("=> Basic String functionality:");
+            string firstName = "Freddy";
+            Console.WriteLine("Value of firstName: {0}", firstName);
+            Console.WriteLine("firstName has {0} characters.", firstName.Length);
+            Console.WriteLine("firstName in uppercase: {0}", firstName.ToUpper());
+            Console.WriteLine("firstName in lowercase: {0}", firstName.ToLower());
+            Console.WriteLine("firstName contains the letter y?: {0}",
+                firstName.Contains("y"));
+            Console.WriteLine("firstName after replace: {0}", firstName.Replace("dy", ""));
+            Console.WriteLine();
+        }
+
+        static void JoachimStringFunctionality()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
