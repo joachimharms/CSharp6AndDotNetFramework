529e5a6287f728dfdf1777ae2059cea8b6d14097
joachimharms <joachimharms77@gmail.com>
2017-11-29 23:21:28 +0100

    Escaping - done

--- C1_001_AssemblyAndManifest/C03_004_MembersOfSystemString/Program.cs
+++ C1_001_AssemblyAndManifest/C03_004_MembersOfSystemString/Program.cs
@@ -11,6 +11,10 @@ namespace C03_004_MembersOfSystemString
         static void Main(string[] args)
         {
             BasicStringFunctionality();
+            StringConcatenation();
+            EscapeChars();
+
+            JoachimStringFunctionality();
         }
 
         static void BasicStringFunctionality()
@@ -21,15 +25,50 @@ namespace C03_004_MembersOfSystemString
             Console.WriteLine("firstName has {0} characters.", firstName.Length);
             Console.WriteLine("firstName in uppercase: {0}", firstName.ToUpper());
             Console.WriteLine("firstName in lowercase: {0}", firstName.ToLower());
-            Console.WriteLine("firstName contains the letter y?: {0}",
-                firstName.Contains("y"));
+            Console.WriteLine("firstName contains the letter y?: {0}", firstName.Contains("y"));
             Console.WriteLine("firstName after replace: {0}", firstName.Replace("dy", ""));
             Console.WriteLine();
         }
 
         static void JoachimStringFunctionality()
         {
-            throw new NotImplementedException();
+            string myString = "Full power";
+            string myOtherString = "Awesome";
+            string myTestString = "blabla";
+
+            Console.WriteLine(myString.Contains("l"));
+            Console.WriteLine(String.Compare(myString, myOtherString));
+            Console.WriteLine(myString.Equals(myOtherString));
+
+            Console.WriteLine(myTestString.Insert(3, myOtherString));
+            Console.WriteLine(myString.PadLeft(15, '0'));
+
+            Console.WriteLine(myString.Remove(5, 5));
+
+
+        }
+
+        static void StringConcatenation()
+        {
+            Console.WriteLine("=> String concatenation:");
+            string s1 = "Programming the ";
+            string s2 = "PsychoDrill (PTP)";
+            string s3 = s1 + s2;
+            Console.WriteLine(s3);
+            Console.WriteLine();
+        }
+
+        static void EscapeChars()
+        {
+            Console.WriteLine("=> Escape characters:\a");
+            string strWithTabs = "Model\tColor\tSpeed\tPet Name\a ";
+            Console.WriteLine(strWithTabs);
+            Console.WriteLine("Everyone loves \"Hello World\"\a ");
+            Console.WriteLine("C:\\MyApp\\bin\\Debug\a ");
+            // Adds a total of 4 blank lines (then beep again!).
+            Console.WriteLine("All finished.\n\n\n\a ");
+            Console.WriteLine();
+
         }
     }
 }
