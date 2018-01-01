60431882a02321152d6646b88bcf4f8710e602c7
joachimharms <joachimharms77@gmail.com>
2018-01-01 11:22:47 +0100

    Finish Coalescing and Conditional Null Operator - done

--- C1_001_AssemblyAndManifest/C04_008_NullableTypes/Program.cs
+++ C1_001_AssemblyAndManifest/C04_008_NullableTypes/Program.cs
@@ -58,7 +58,52 @@ namespace C04_008_NullableTypes
             else
                 Console.WriteLine("Value of 'b' is undefined.");
             Console.ReadLine();
+            Console.WriteLine();
 
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine("The Null Coalescing Operator");
+
+            DatabaseReader dr2 = new DatabaseReader();
+            // If the value from GetIntFromDatabase() is null,
+            // assign local variable to 100.
+            int myData = dr2.GetIntFromDatabase() ?? 100;
+            Console.WriteLine("Value of myData: {0}", myData);
+            Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("If else Bedingung als umständlicher Ersatz für ?? Operator");
+            // Long-hand notation not using ?? syntax.
+            int? moreData = dr.GetIntFromDatabase();
+            if (!moreData.HasValue)
+                moreData = 100;
+            Console.WriteLine("Value of moreData: {0}", moreData);
+            Console.WriteLine();
+
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.WriteLine("Check auf Null mit Coalescing Operator and Conditional Null Operator:");
+            string[] testerStringArr = new string[3] { null, null, null };
+            string[] testerStringArr2 = null;
+
+            Console.WriteLine("*****Array ist nicht null:****");
+
+            TesterMethod(testerStringArr);
+            Console.WriteLine();
+
+            TesterMethod2(testerStringArr);
+            Console.WriteLine();
+
+            TesterMethod3(testerStringArr);
+            Console.WriteLine();
+
+            Console.WriteLine("*****Array ist zunächst null:****");
+            TesterMethod(testerStringArr2);
+            Console.WriteLine();
+
+            TesterMethod2(testerStringArr2);
+            Console.WriteLine();
+
+            TesterMethod3(testerStringArr2);
+            Console.WriteLine();
         }
 
         static void LocalNullableVariables()
@@ -81,6 +126,29 @@ namespace C04_008_NullableTypes
             Nullable<bool> nullableBool = null;
             Nullable<char> nullableChar = 'a';
             Nullable<int>[] arrayOfNullableInts = new Nullable<int>[10];
+        }
+
+        static void TesterMethod(string[] args)
+        {
+            // We should check for null before accessing the array data!
+            if (args != null)
+            {
+                Console.WriteLine("Checking for null on traditionell way");
+                Console.WriteLine($"You sent me {args.Length} arguments.");
+            }
+        }
+
+        static void TesterMethod2(string[] args)
+        {
+            // We should check for null before accessing the array data!
+            Console.WriteLine("Check for null with coalescing operator");
+            Console.WriteLine($"You sent me {args?.Length} arguments.");
+        }
+
+        static void TesterMethod3(string[] args)
+        {
+            Console.WriteLine("Conditional Null Operator and Coalescing Operator zusammen:");
+            Console.WriteLine($"You sent me {args?.Length ?? 0} arguments.");
         }
     }
 }
