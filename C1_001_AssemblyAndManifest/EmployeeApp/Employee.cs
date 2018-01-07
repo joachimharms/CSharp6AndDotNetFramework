c1b3541b946432eb84660fa9bc13d00b317f428b
joachimharms <joachimharms77@gmail.com>
2018-01-07 20:36:39 +0100

    .NET properties -done

--- C1_001_AssemblyAndManifest/EmployeeApp/Employee.cs
+++ C1_001_AssemblyAndManifest/EmployeeApp/Employee.cs
@@ -13,40 +13,102 @@ namespace EmployeeApp
 
         private float currPay;
 
+        // New field and property.
+        private int empAge;
+        public int Age
+        {
+            get { return empAge; }
+            set { empAge = value; }
+        }
+
         #endregion
 
         #region Getter and Setter
 
-        // Accessor (get method).
-        public string GetName()
+        //// Traditional get and set methods:
+        //// Accessor (get method).
+        //public string GetName()
+        //{
+        //    return this.empName;
+        //}
+        //// Mutator (set method).
+        //public void SetName(string name)
+        //{
+        //    // Do a check on incoming value
+        //    // before making assignment.
+        //    if (name.Length > 15)
+        //        Console.WriteLine("Error! Name length exceeds 15 characters!");
+        //    else
+        //        this.empName = name;
+        //}
+
+
+        // .NET Properties!
+        public string Name
         {
-            return this.empName;
-        }
-        // Mutator (set method).
-        public void SetName(string name)
-        {
-            // Do a check on incoming value
-            // before making assignment.
-            if (name.Length > 15)
-                Console.WriteLine("Error! Name length exceeds 15 characters!");
-            else
-                this.empName = name;
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Error! Name length exceeds 15 characters!");
+                else
+                    empName = value;
+            }
         }
 
+        // The 'int' represents the type of data this property encapsulates.
+        // Notice the lack of parantheses
+        public int ID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+        public float Pay
+        {
+            get { return currPay; }
+            set { currPay = value; }
+        }
         #endregion
 
         #region Constructors
 
-        public Employee()
-        {
+        //public Employee()
+        //{
 
-        }
+        //}
+
+        //public Employee(string name, int id, float pay)
+        //{
+        //    this.empName = name;
+        //    this.empID = id;
+        //    this.currPay = pay;
+        //}
+
+        // Updated constructors.
+        public Employee() { }
 
         public Employee(string name, int id, float pay)
+            : this(name, 0, id, pay) { }
+
+        public Employee(string name, int age, int id, float pay)
         {
-            this.empName = name;
-            this.empID = id;
-            this.currPay = pay;
+            ////empName = name;
+            //// Humm, this seems like a problem...
+            //if (name.Length > 15)
+            //    Console.WriteLine("Error! Name length exceeds 15 characters!");
+            //else
+            //    empName = name;
+
+            // Better! Use properties when setting class data.
+            // This reduces the amount of duplicate error checks.
+            Name = name;
+            Age = age;
+            ID = id;
+            Pay = pay;
+
+            //empID = id;
+            //empAge = age;
+            //currPay = pay;
         }
         #endregion
 
@@ -54,14 +116,32 @@ namespace EmployeeApp
 
         public void GiveBonus(float amount)
         {
-            this.currPay += amount;
+            //this.currPay += amount;
+            Pay += amount;
         }
+
+        //public void DisplayStats()
+        //{
+        //    Console.WriteLine("Name: {0}", empName);
+        //    Console.WriteLine("ID: {0}", empID);
+        //    Console.WriteLine("Pay: {0}", currPay);
+        //}
+
+        //// Updated DisplayStats() method now accounts for age.
+        //public void DisplayStats()
+        //{
+        //    Console.WriteLine("Name: {0}", empNam);
+        //    Console.WriteLine("ID: {0}", empID);
+        //    Console.WriteLine("Age: {0}", empAge);
+        //    Console.WriteLine("Pay: {0}", currPay);
+        //}
 
         public void DisplayStats()
         {
-            Console.WriteLine("Name: {0}", empName);
-            Console.WriteLine("ID: {0}", empID);
-            Console.WriteLine("Pay: {0}", currPay);
+            Console.WriteLine("Name: {0}", Name);
+            Console.WriteLine("ID: {0}", ID);
+            Console.WriteLine("Age: {0}", Age);
+            Console.WriteLine("Pay: {0}", Pay);
         }
         #endregion
     }
--- C1_001_AssemblyAndManifest/EmployeeApp/Program.cs
+++ C1_001_AssemblyAndManifest/EmployeeApp/Program.cs
@@ -10,24 +10,41 @@ namespace EmployeeApp
     {
         static void Main(string[] args)
         {
-            //Employee emp = new Employee();
-            //// Error! Cannot directly access private members
-            //// from an object!
-            //emp.empName = "Marv";
+            ////Employee emp = new Employee();
+            ////// Error! Cannot directly access private members
+            ////// from an object!
+            ////emp.empName = "Marv";
+
+            //Console.WriteLine("***** Fun with Encapsulation *****\n");
+            //Employee emp = new Employee("Marvin", 456, 30000);
+            //emp.GiveBonus(1000);
+            //emp.DisplayStats();
+
+
+            ////// Use the get/set methods to interact with the object's name.
+            //////emp.SetName("Marv");
+            //// Replaced getter and setters with properties:
+
+            //emp.Name = "Marv";
+            ////Console.WriteLine("Employee is named: {0}", emp.GetName());
+            //Console.WriteLine("Employee is named: {0}", emp.Name);
+
+            //Console.ReadLine();
+
+            //// Longer than 15 characters! Error will print to console.
+            //Employee emp2 = new Employee();
+            ////emp2.SetName("Xena the warrior princess");
+            //emp2.Name = "Xena the warrior princess";
+            //Console.ReadLine();
+
 
             Console.WriteLine("***** Fun with Encapsulation *****\n");
             Employee emp = new Employee("Marvin", 456, 30000);
             emp.GiveBonus(1000);
             emp.DisplayStats();
-
-            // Use the get/set methods to interact with the object's name.
-            emp.SetName("Marv");
-            Console.WriteLine("Employee is named: {0}", emp.GetName());
-            Console.ReadLine();
-
-            // Longer than 15 characters! Error will print to console.
-            Employee emp2 = new Employee();
-            emp2.SetName("Xena the warrior princess");
+            // Reset and then get the Name property.
+            emp.Name = "Marv";
+            Console.WriteLine("Employee is named: {0}", emp.Name);
             Console.ReadLine();
         }
     }
