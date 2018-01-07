b0e35ec4577e6fc29e4b6c45c5cafc767d20ef57
joachimharms <joachimharms77@gmail.com>
2018-01-07 11:04:50 +0100

    Implement traditional get and set methods -done

---
+++ C1_001_AssemblyAndManifest/EmployeeApp/Employee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+
+namespace EmployeeApp
+{
+    public class Employee
+    {
+        #region Field Data
+
+        private string empName;
+
+        private int empID;
+
+        private float currPay;
+
+        #endregion
+
+        #region Getter and Setter
+
+        // Accessor (get method).
+        public string GetName()
+        {
+            return this.empName;
+        }
+        // Mutator (set method).
+        public void SetName(string name)
+        {
+            // Do a check on incoming value
+            // before making assignment.
+            if (name.Length > 15)
+                Console.WriteLine("Error! Name length exceeds 15 characters!");
+            else
+                this.empName = name;
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string name, int id, float pay)
+        {
+            this.empName = name;
+            this.empID = id;
+            this.currPay = pay;
+        }
+        #endregion
+
+        #region Methods
+
+        public void GiveBonus(float amount)
+        {
+            this.currPay += amount;
+        }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", empName);
+            Console.WriteLine("ID: {0}", empID);
+            Console.WriteLine("Pay: {0}", currPay);
+        }
+        #endregion
+    }
+}
