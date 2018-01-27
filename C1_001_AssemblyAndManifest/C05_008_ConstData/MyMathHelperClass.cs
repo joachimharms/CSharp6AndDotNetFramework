db99448ec73756bdf89a087cf500a7e3ea7e28a1
joachimharms <joachimharms77@gmail.com>
2018-01-27 10:44:48 +0100

    Finish const and readonly data fields - done

---
+++ C1_001_AssemblyAndManifest/C05_008_ConstData/MyMathHelperClass.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace C05_008_ConstData
+{
+    public class MyMathHelperClass
+    {
+        //public const double Pi = 3.14;
+
+
+        //// Read-only fields can be assigned in ctors,
+        //// but nowhere else.
+        //public readonly double Pi;
+        //public MyMathHelperClass()
+        //{
+        //    Pi = 3.14;
+        //}
+
+        //// static readonly Field:
+        //public static readonly double Pi = 3.14;
+
+        // static readonly Field mit Wertezuweisung über einen statischen Konstruktor
+        public static readonly double Pi;
+        static MyMathHelperClass()
+        { Pi = 3.14; }
+
+    }
+
+}
+
---
+++ C1_001_AssemblyAndManifest/C05_008_ConstData/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C05_008_ConstData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Const *****\n");
+            //Console.WriteLine("The value of PI is: {0}", MyMathHelperClass.Pi);
+            // Error! Can't change a constant!
+            // MyMathClass.PI = 3.1444;
+            Console.ReadLine();
+
+
+            //Console.WriteLine("Readonly Data:");
+            //MyMathHelperClass mathHelper = new MyMathHelperClass();
+            //Console.WriteLine("The value of PI is: {0}", mathHelper.Pi);
+            //Console.ReadLine();
+
+            Console.WriteLine("static Readonly Data mit Werte Zuweisung zur Laufzeit über einen statischen Konstruktor: ");
+            MyMathHelperClass mathHelper = new MyMathHelperClass();
+            Console.WriteLine("The value of PI is: {0}", MyMathHelperClass.Pi);
+        }
+    }
+}
