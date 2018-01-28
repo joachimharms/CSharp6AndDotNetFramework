40ffd3d6186edac2436ac7ac91c15331241b360d
joachimharms <joachimharms77@gmail.com>
2018-01-28 13:32:56 +0100

    Basic Inheritance and Classdesign diagram - done

---
+++ C1_001_AssemblyAndManifest/C06_001_BasicInheritance/TestDesignerClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C06_001_BasicInheritance
+{
+    public class TestDesignerClass
+    {
+    }
+}
