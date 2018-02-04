89d5aacb0f0ab001ce8e37a8e46b9616124241cd
joachimharms <joachimharms77@gmail.com>
2018-02-04 12:05:25 +0100

    Class Type Casting -done

---
+++ C1_001_AssemblyAndManifest/C06_011_CastingClasses/Employee1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Globalization;
+namespace C06_011_CastingClasses
+{
+
+    // Employee.cs - eher häufig zu ändernde Daten
+    // Update the Employee class as abstract
+    // to prevent direct instantiation.
+    public abstract partial class Employee
+    {
+        #region Methods
+
+        //public void GiveBonus(float amount)
+        //{
+        //    //this.currPay += amount;
+        //    Pay += amount;
+        //}
+
+        // This method can now be "overridden" by a derived class.
+        public virtual void GiveBonus(float amount)
+        {
+            Pay += amount;
+        }
+
+        //public void DisplayStats()
+        //{
+        //    Console.WriteLine("Name: {0}", empName);
+        //    Console.WriteLine("ID: {0}", empID);
+        //    Console.WriteLine("Pay: {0}", currPay);
+        //}
+
+        //// Updated DisplayStats() method now accounts for age.
+        //public void DisplayStats()
+        //{
+        //    Console.WriteLine("Name: {0}", empNam);
+        //    Console.WriteLine("ID: {0}", empID);
+        //    Console.WriteLine("Age: {0}", empAge);
+        //    Console.WriteLine("Pay: {0}", currPay);
+        //}
+
+        public virtual void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", Name);
+            Console.WriteLine("ID: {0}", ID);
+            //Console.WriteLine("Age: {0}", Age);
+            Console.WriteLine("Pay: {0}", Pay);
+        }
+        #endregion
+
+        #region Getter and Setter
+
+        //// Traditional get and set methods:
+        //// Accessor (get method).
+        //public string GetName()
+        //{
+        //    return this.empName;
+        //}
+        //// Mutator (set method).
+        //public void SetName(string name)
+        //{
+        //    // Do a check on incoming value
+        //    // before making assignment.
+        //    if (name.Length > 15)
+        //        Console.WriteLine("Error! Name length exceeds 15 characters!");
+        //    else
+        //        this.empName = name;
+        //}
+
+
+        // .NET Properties!
+        public string Name
+        {
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Error! Name length exceeds 15 characters!");
+                else
+                    empName = value;
+            }
+        }
+
+        // The 'int' represents the type of data this property encapsulates.
+        // Notice the lack of parantheses
+        public int ID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+        public float Pay
+        {
+            get { return currPay; }
+            set { currPay = value; }
+        }
+        #endregion
+    }
+}
---
+++ C1_001_AssemblyAndManifest/C06_011_CastingClasses/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C06_011_CastingClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A Manager "is-a" System.Object, so we can
+            // store a Manager reference in an object variable just fine.
+            object frank = new Manager("Frank Zappa", 9, 3000, 40000, "111-11-1111", 5);
+
+            // A Manager "is-an" Employee too.
+            Employee moonUnit = new Manager("MoonUnit Zappa", 2, 3001, 20000, "101-11-1321", 1);
+            GivePromotion(moonUnit);
+
+            // A PTSalesPerson "is-a" SalesPerson.
+            SalesPerson jill = new PTSalesPerson("Jill", 834, 3002, 100000, "111-12-1119", 90);
+            GivePromotion(jill);
+
+
+            //object frank2 = new Manager("Frank Zappa", 9, 3000, 40000, "111-11-1111", 5);
+            //// Error!
+            //GivePromotion(frank2);
+
+            // OK! Explizites Downcasting von System.Object in Manager:
+            GivePromotion((Manager)frank);
+        }
+
+        static void GivePromotion(Employee emp)
+        {
+            // Increase pay...
+            // Give new parking space in company garage...
+            Console.WriteLine("{0} was promoted!", emp.Name);
+        }
+    }
+}
