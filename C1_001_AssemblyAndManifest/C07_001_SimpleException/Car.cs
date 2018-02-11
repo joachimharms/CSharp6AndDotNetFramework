bae66b127a2454d9b00ee4e2c38675ebf6b91561
joachimharms <joachimharms77@gmail.com>
2018-02-11 04:28:26 +0100

    Implement try/catch block -done ToDo: Anki cards for topic try/catch logic

---
+++ C1_001_AssemblyAndManifest/C07_001_SimpleException/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C07_001_SimpleException
+{
+    class Car
+    {
+        // Konstante für Höchstgeschwindigkeit.
+        public const int MaxSpeed = 100;
+
+        // Car properties.
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+
+        // Is the car still operational?
+        private bool carlIsDead;
+
+        // A car has a radio.
+        private Radio theMusicBox = new Radio();
+
+        // Constructors
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            this.CurrentSpeed = speed;
+            this.PetName = name;
+        }
+
+        // Delegate request to inner object.
+        public void CrankTunes(bool state)
+        {
+            this.theMusicBox.TurnOn(state);
+        }
+
+        // See if Car has overheated.
+        public void Accelerate(int delta)
+        {
+            if (carlIsDead)
+            {
+                Console.WriteLine($"{this.PetName} is out of order...");
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    //Console.WriteLine($"{this.PetName} has overheated!");
+                    CurrentSpeed = 0;
+                    carlIsDead = true;
+
+                    // Use the "throw" keyword to raise an exception.
+                    throw new Exception(string.Format($"{this.PetName} has overheated!"));
+                }
+                else
+                {
+                    Console.WriteLine($"=> Current Speed = {CurrentSpeed}");
+                }
+            }
+        }
+
+    }
+}
---
+++ C1_001_AssemblyAndManifest/C07_001_SimpleException/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C07_001_SimpleException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("***** Simple Exception Example *****");
+            //Console.WriteLine("=> Creating a car and stepping on it!");
+            //Car myCar = new Car("Zippy", 20);
+            //myCar.CrankTunes(true);
+            //for (int i = 0; i < 10; i++)
+            //    myCar.Accelerate(10);
+            //Console.ReadLine();
+
+            // Handle the thrown exception.
+            Console.WriteLine("***** Simple Exception Example *****");
+            Console.WriteLine("=> Creating a car and stepping on it!");
+            Car myCar = new Car("Zippy", 20);
+            myCar.CrankTunes(true);
+
+            // Speed up past the car's max speed to
+            // trigger the exception.
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    myCar.Accelerate(10);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n*** Error! ***");
+                Console.WriteLine($"Method: {e.TargetSite}");
+                Console.WriteLine($"Message: {e.Message}");
+                Console.WriteLine($"Source: {e.Source}");
+            }
+
+            // The error has been handled, processing continues with the next statement.
+            Console.WriteLine("\n***** Out of exception logic *****");
+            Console.ReadLine();
+
+        }
+    }
+}
