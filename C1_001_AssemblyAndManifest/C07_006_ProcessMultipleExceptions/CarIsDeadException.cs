d779178918ba4866b26627ae8e71c76430c05354
joachimharms <joachimharms77@gmail.com>
2018-02-18 22:16:26 +0100

    Begin ProcessMultipleExceptions Project -done ToDo: Finish Project and write quiz cards

---
+++ C1_001_AssemblyAndManifest/C07_006_ProcessMultipleExceptions/CarIsDeadException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C07_006_ProcessMultipleExceptions
+{
+    [Serializable]
+    public class CarIsDeadException : ApplicationException
+    {
+        public CarIsDeadException() { }
+        public CarIsDeadException(string message) : base(message) { }
+        public CarIsDeadException(string message,
+        System.Exception inner)
+        : base(message, inner) { }
+        protected CarIsDeadException(
+        System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context)
+        : base(info, context) { }
+
+        // Any additional custom properties, constructors and data members...
+        public DateTime ErrorTimeStamp { get; set; }
+        public string CauseOfError { get; set; }
+
+        public CarIsDeadException(string message,
+        string cause, DateTime time) : base(message)
+        {
+            CauseOfError = cause;
+            ErrorTimeStamp = time;
+        }
+
+
+    }
+}
---
+++ C1_001_AssemblyAndManifest/C07_006_ProcessMultipleExceptions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C07_006_ProcessMultipleExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Handling Multiple Exceptions *****\n");
+            Car myCar = new Car("Rusty", 90);
+            try
+            {
+                // Trip Arg out of range exception.
+                myCar.Accelerate(-10);
+            }
+            catch (CarIsDeadException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
