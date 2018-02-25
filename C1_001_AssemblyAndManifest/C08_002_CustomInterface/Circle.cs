76bbfc930f45dcf5dd69f55f723962e5053f8634
joachimharms <joachimharms77@gmail.com>
2018-02-25 14:59:07 +0100

    Implement project CustomInterface -done

---
+++ C1_001_AssemblyAndManifest/C08_002_CustomInterface/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C08_002_CustomInterface
+{
+    // Circle DOES NOT override Draw().
+    class Circle : Shape
+    {
+        public Circle() { }
+
+        public Circle(string name) : base(name) { }
+
+    }
+}
---
+++ C1_001_AssemblyAndManifest/C08_002_CustomInterface/Hexagon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C08_002_CustomInterface
+{
+    class Hexagon : Shape
+    {
+        public Hexagon() { }
+
+        public Hexagon(string name) : base(name) { }
+
+        public override void Draw()
+        {
+            Console.WriteLine($"Drawing the {base.PetName} Hexagon");
+        }
+    }
+}
